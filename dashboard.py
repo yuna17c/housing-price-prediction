c353197f974226e9b783cef33e58b0f98a8732db
Yuna <yuna0617c@gmail.com>
2024-06-10 10:42:14 -0400

    add streamlit app

--- dashboard.py
+++ dashboard.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pickle
 import joblib
 import numpy as np
+import pandas as pd
 from stacking_model_class import StackingAveragedModels
 from sklearn.preprocessing import MinMaxScaler
 
@@ -17,20 +18,37 @@ with open('xgb_model.pkl', 'rb') as file:
 st.title('Predict your house price!')
 
 st.header('Input')
-f1 = st.number_input('Area')
+f1 = st.number_input('Overall Area')
 f2 = st.number_input('Basement Area')
 f3 = st.number_input('Garage Area')
-f4 = st.number_input('Year Built')
-f5 = st.number_input('Overall Quality')
-f6 = st.number_input('Overall Condition')
+f7 = st.number_input('Number of Bathrooms',step=0.5)
+f4 = st.number_input('Year Built',min_value=1879, max_value=2024, step=1)
+# overall quality selection
+options = list(range(1, 11))
+selected_qual = st.selectbox('Overall Quality:', options)
+# neighborhood selection
+options = ['NAmes', 'Gilbert', 'StoneBr', 'BrDale', 'NPkVill', 'NridgHt',
+       'Blmngtn', 'NoRidge', 'Somerst', 'SawyerW', 'Sawyer', 'NWAmes',
+       'OldTown', 'BrkSide', 'ClearCr', 'SWISU', 'Edwards', 'CollgCr',
+       'Crawfor', 'Blueste', 'IDOTRR', 'Mitchel', 'Timber', 'MeadowV',
+       'Veenker']
+selected_neigh = st.selectbox('Neighborhood:', options)
 
 if st.button('Predict'):
-    input_features = [[f1, f2, f3, f4, f5, f6]]
+    df = pd.read_csv("Data/test.csv")
     x = np.array([22.583655055810706, 11.198636321275528, 7, 2.290115055569869, 1992, 1992, -13.884279591841091, 10.378762467643101, -368.4565890213832, 10.609035186067386, 4.73306922547225, 6.805844797967125, 5.684399178682556, -369056525457388.9, 9.977432868874661, 9.999442302538419e-05, -26476949.38372713, 2, 9.999414614347741e-05, 3, 9.999692328757053e-05, 2.6740307857691086, 9.999516239672483e-05, 2, 564, 5.191454825504756, 3.9355608616562465, -189.13678177493756, -1.7063697605496562e+16, -3596.9451496908405, -1.5064526509977826e+61, -7325809.697544301, 43.98523673614452, 3.5, 1, 12, 1, False, False, False, False, False, False, False, False, False, True, False, False, False, False, False, False, False, False, True, False, False, True, False, True, False, True, False, False, False, False, False, False, True, True, False, False, False, False, False, True, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, True, False, False, False, False, False, False, False, False, False, False, False, True, False, False, False, False, False, False, False, False, True, False, False, False, False, False, True, False, False, False, False, False, False, False, False, False, True, False, False, False, True, False, False, False, False, True, False, False, False, False, False, False, False, False, False, False, False, False, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, True, False, False, False, False, False, False, False, False, False, False, False, True, False, False, False, True, False, False, False, False, False, True, False, False, True, False, False, False, False, False, True, False, False, False, False, False, False, True, False, False, False, False, True, False, False, True, False, False, False, False, False, False, False, False, False, False, True, False, True, False, False, False, False, True, False, False, False, False, False, True, False, False, False, False, True, False, False, True, False, False, False, False, False, False, False, True, False, False, False, False, False, True, False, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, True, False, False, False, False, False, False, True, False, False, False, False, False, True, False, False, True, False, False, False, False, True, False, True, False, False, False, False, False, False, False, False, False, False, False, True, False, False, False, False, False, False, False, True, False, False, False, False, False, False, False, False, True, False, False, False, False, True, False])
+
+    input_features = [f1, f2, f3, f7, f4, selected_qual]
+    col_names = ['GrLivArea','TotalBsmtSF', 'GarageArea','FullBath','YearBuilt','OverallQual']
+    for col,user_input in zip(col_names,input_features):
+        idx = df.columns.get_loc(col)
+        x[idx] = user_input
+    x[92] = 1
+
     x_scaled = scaler.fit_transform(x.reshape(1,-1))
     xgb_pred = np.expm1(xgb_model.predict(x_scaled))
     stacked_pred = np.expm1(stacked_model.predict(x_scaled))
     ensemble_pred = stacked_pred*0.70 + xgb_pred*0.3
-    st.subheader('Predicted Output')
+    st.subheader('Predicted Price')
     val = '$'+str(round(ensemble_pred[0],2))
     st.write(val)
--- stacking_model_class.py
+++ stacking_model_class.py
@@ -8,14 +8,11 @@ class StackingAveragedModels(BaseEstimator, TransformerMixin, RegressorMixin):
         self.meta_model = meta_model
         self.n_folds = n_folds
    
-    # We again fit the data on clones of the original models
     def fit(self, X, y):
         self.base_models_ = [list() for x in self.base_models]
         self.meta_model_ = clone(self.meta_model)
         kfold = KFold(n_splits=self.n_folds, shuffle=True, random_state=156)
         
-        # Train cloned base models then create out-of-fold predictions
-        # that are needed to train the cloned meta-model
         out_of_fold_predictions = np.zeros((X.shape[0], len(self.base_models)))
         for i, model in enumerate(self.base_models):
             for train_index, holdout_index in kfold.split(X, y):
@@ -25,12 +22,9 @@ class StackingAveragedModels(BaseEstimator, TransformerMixin, RegressorMixin):
                 y_pred = instance.predict(X[holdout_index,:])
                 out_of_fold_predictions[holdout_index, i] = y_pred
                 
-        # Now train the cloned  meta-model using the out-of-fold predictions as new feature
         self.meta_model_.fit(out_of_fold_predictions, y)
         return self
    
-    #Do the predictions of all base models on the test data and use the averaged predictions as 
-    #meta-features for the final prediction which is done by the meta-model
     def predict(self, X):
         meta_features = np.column_stack([
             np.column_stack([model.predict(X) for model in base_models]).mean(axis=1)
